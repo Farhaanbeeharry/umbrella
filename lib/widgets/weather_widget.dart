e08321710d2573618b37ea612a3cb035c0fb0ddf
Farhaan Beeharry <farhaanbeeharry.ms@gmail.com>
2022-10-03 11:58:09 +0400

    Added on hover to weather widgets

--- lib/widgets/weather_widget.dart
+++ lib/widgets/weather_widget.dart
@@ -21,48 +21,69 @@ class WeatherWidget extends StatefulWidget {
 }
 
 class _WeatherWidgetState extends State<WeatherWidget> {
+  bool _isHovering = false;
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
         widget.onClick(widget.id, widget.timeOfTheDay, widget.label);
       },
-      child: AnimatedContainer(
-        width: MediaQuery.of(context).size.width * 0.08,
-        height: MediaQuery.of(context).size.height * 0.17,
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(
-            Radius.circular(
-              10.0,
-            ),
-          ),
-          border:
-              widget.selected == widget.label ? Border.all(color: Colors.white) : null,
-          color: widget.selected == widget.label
-              ? const Color.fromRGBO(217, 217, 217, 0.05)
-              : null,
-        ),
-        duration: const Duration(milliseconds: 100),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/images/weather/${widget.label}.png",
-              width: MediaQuery.of(context).size.width * 0.035,
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              widget.label,
-              style: TextStyle(
-                fontFamily: Poppins.regular,
-                color: Colors.white,
-                fontSize: 14.0,
+      child: MouseRegion(
+        onHover: (event) {
+          setState(() {
+            _isHovering = true;
+          });
+        },
+        onExit: (event) {
+          _isHovering = false;
+        },
+        child: AnimatedContainer(
+          width: MediaQuery.of(context).size.width * 0.08,
+          height: MediaQuery.of(context).size.height * 0.17,
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(
+              Radius.circular(
+                10.0,
               ),
-            )
-          ],
+            ),
+            border: _isHovering
+                ? (widget.selected == widget.label
+                    ? Border.all(color: Colors.white)
+                    : Border.all(color: Colors.white30))
+                : (widget.selected == widget.label
+                    ? Border.all(color: Colors.white)
+                    : null),
+            color: _isHovering
+                ? (widget.selected == widget.label
+                    ? const Color.fromRGBO(217, 217, 217, 0.05)
+                    : const Color.fromRGBO(150, 150, 150, 0.03))
+                : (widget.selected == widget.label
+                    ? const Color.fromRGBO(217, 217, 217, 0.05)
+                    : null),
+          ),
+          duration: const Duration(milliseconds: 100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/weather/${widget.label}.png",
+                width: MediaQuery.of(context).size.width * 0.035,
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                widget.label,
+                style: TextStyle(
+                  fontFamily: Poppins.regular,
+                  color: Colors.white,
+                  fontSize: 14.0,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
