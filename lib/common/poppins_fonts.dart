5cb2a7f9a8047eabf9c1c0b2bea9642882109e82
Farhaan Beeharry <farhaanbeeharry.ms@gmail.com>
2022-10-03 14:59:49 +0400

    Fixed performance issue

--- lib/common/poppins_fonts.dart
+++ lib/common/poppins_fonts.dart
@@ -1,20 +1,6 @@
 class Poppins {
-  static String black = "PoppinsBlack";
-  static String blackItalic = "PoppinsBlackItalic";
   static String bold = "PoppinsBold";
-  static String boldItalic = "PoppinsBoldItalic";
-  static String extraBold = "PoppinsExtraBold";
-  static String extraBoldItalic = "PoppinsExtraBoldItalic";
-  static String extraLight = "PoppinsExtraLight";
-  static String extraLightItalic = "PoppinsExtraLightItalic";
-  static String italic = "PoppinsItalic";
   static String light = "PoppinsLight";
-  static String lightItalic = "PoppinsLightItalic";
-  static String medium = "PoppinsMedium";
-  static String mediumItalic = "PoppinsMediumItalic";
   static String regular = "PoppinsRegular";
-  static String semiBold = "PoppinsSemiBold";
-  static String semiBoldItalic = "PoppinsSemiBoldItalic";
   static String thin = "PoppinsThin";
-  static String thinItalic = "PoppinsThinItalic";
 }
